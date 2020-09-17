3ec908e600bdb90e45faca35b5bed3f4a9e9375c
IrisDuMutel <irisfp96@gmail.com>
2020-09-17 17:38:11 +0200

    not using trajplan, problems message_filters

--- scripts/trajplan.py
+++ scripts/trajplan.py
@@ -11,12 +11,12 @@ def navigator():
     rospy.init_node('trajplan',anonymous=True) # Create goal node called 'trajplan'
     pub = rospy.Publisher('ref_vel',vect_msg,queue_size=10) #create goal publisher in the topic ref_vel, with vector_msg msgs
     # and goal queue_size (size of outgoing message) of 10
-    rate = rospy.Rate(1) # 1hz
+    # rate = rospy.Rate(1) # 1hz
     rospy.loginfo("Status: Initialized") 
     # Subscribe to messages
     rs_sub = message_filters.Subscriber('/rs_vect', vect_msg)
     # Syncronize
-    ts = message_filters.ApproximateTimeSynchronizer([rs_sub], queue_size=10, slop=0.5)
+    ts = message_filters.TimeSynchronizer([rs_sub], queue_size=10)
     # Register callback and publisher
     ts.registerCallback(callback,pub)
     rospy.spin()
@@ -28,7 +28,9 @@ def callback(rs_sub,pub):
     # bb_vect = vectfcn.sum_vect(vectfcn.sum_vect(vect_a,vect_b),vect_c)
     bb_vect =  rs_v
     # bb_vect = vectfcn.sum_vect(vect_a,vect_b)
-    rospy.loginfo(bb_vect) 
+    rospy.loginfo(bb_vect)
+    print(rs_sub.header.seq) 
+    msg.header.seq = rs_sub.header.seq
     msg.angle = bb_vect[0]
     msg.value = bb_vect[1]
     print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n')

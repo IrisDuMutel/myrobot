2d529f31e66e40fdb137a0e10f1a67632654bdff
IrisDuMutel <irisfp96@gmail.com>
2020-09-22 18:17:27 +0200

    Seems to work fine

--- scripts/realsense.py
+++ scripts/realsense.py
@@ -128,7 +128,7 @@ def callback(color_raw, depth_raw,pub):
             angle = 0
             value = 0
         print(angle, value)
-        vect = [0,0.5]
+        vect = [angle,value]
 
         #FIXME: make a simultaneous visualization of depth and color frames
         if show_depth: 

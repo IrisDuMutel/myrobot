f6f4841365140de17c29a303ba95103082fc3994
IrisDuMutel <irisfp96@gmail.com>
2020-09-24 14:35:04 +0200

    Written the arbiter code

--- scripts/arbiter.py
+++ scripts/arbiter.py
@@ -5,35 +5,35 @@ import math
 import random
 from myrobot.msg import vect_msg
 import message_filters
+import vector_fcn as vectfcn
 
 msg = vect_msg()
 def arbiter():
     rospy.init_node('arbiter',anonymous=True) # Create goal node called 'trajplan'
     pub = rospy.Publisher('ref_vel',vect_msg,queue_size=10) #create goal publisher in the topic ref_vel, with vector_msg msgs
     # and goal queue_size (size of outgoing message) of 10
-    # rate = rospy.Rate(1) # 1hz
-    rospy.loginfo("Status: Initialized") 
-    # Subscribe to messages
+    rospy.loginfo("Status: Arbiter initialized") 
+    # Subscribe to topics
     rs_sub = message_filters.Subscriber('/rs_vect', vect_msg)
+    light_sub = message_filters.Subscriber('/light_vect', vect_msg)
+    force_sub = message_filters.Subscriber('/force_vect', vect_msg)
     # Syncronize
-    ts = message_filters.TimeSynchronizer([rs_sub], queue_size=10)
+    ts = message_filters.TimeSynchronizer([rs_sub,light_sub,force_sub], queue_size=10)
     # Register callback and publisher
     ts.registerCallback(callback,pub)
     rospy.spin()
 
-def callback(rs_sub,pub):
+def callback(rs_sub,light_sub,contact_sub,pub):
     # Collecting vectors
-    rs_v  = [rs_sub.angle, rs_sub.value]
+    vect_a  = [rs_sub.angle, rs_sub.value]
+    vect_b  = [force_sub.angle, force_sub.value]
+    vect_c  = [light_sub.angle, light_sub.value] 
     # Sum vectors
-    # bb_vect = vectfcn.sum_vect(vectfcn.sum_vect(vect_a,vect_b),vect_c)
-    bb_vect =  rs_v
-    # bb_vect = vectfcn.sum_vect(vect_a,vect_b)
-    rospy.loginfo(bb_vect)
-    print(rs_sub.header.seq) 
-    msg.header.seq = rs_sub.header.seq
+    bb_vect = vectfcn.sum_vect(vectfcn.sum_vect(vectfcn.sum_vect(vectfcn.sum_vect(vect_a,vect_b),vect_c),vect_d),vect_e)
+    rospy.loginfo(bb_vect) 
+    # Publish message to topic
     msg.angle = bb_vect[0]
     msg.value = bb_vect[1]
-    print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n')
     pub.publish(msg)
 
 if  __name__ == '__main__':

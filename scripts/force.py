441dc30b7f9296f55e1a759d17f31b3608f8e8d8
IrisDuMutel <irisfp96@gmail.com>
2020-09-28 17:17:21 +0200

    Working on force sensors

--- scripts/force.py
+++ scripts/force.py
@@ -12,39 +12,40 @@ def force():
     rospy.init_node('force',anonymous=True)
     rospy.loginfo("Status: Force Sensors Initialized") 
     # Subscribe to topics
-    front_sub = message_filters.Subscriber('/force/front_sensor', ContactsState)
-    back_sub = message_filters.Subscriber('/force/back_sensor', ContactsState)
+    rightfront_sub = message_filters.Subscriber('/force/rightfront_sensor', ContactsState)
+    leftfront_sub = message_filters.Subscriber('/force/leftfront_sensor', ContactsState)
     # Syncronize
-    ts = message_filters.TimeSynchronizer([front_sub,back_sub], queue_size=10)
+    ts = message_filters.TimeSynchronizer([rightfront_sub,leftfront_sub], queue_size=10)
     # Register callback and publisher
     ts.registerCallback(callback)
     rospy.spin()
 
-def callback(front_sub,back_sub):
+def callback(rightfront_sub,leftfront_sub):
     msg = vect_msg()
     #TODO define here a behavior according to the contact value
     # angle = (forceL - forceR) * 180/1023
     # force = (forceL+forceR)/2
+    msg.value=0.4
     angle = 0
-    if not front_sub.states:
+    if not rightfront_sub.states:
         pass
     else:
-        front_force = front_sub.states[0].total_wrench.force.x
-        rospy.loginfo('Ive been toched at the front F=%s\n', front_force)
+        rightfront_force = rightfront_sub.states[0].total_wrench.force.x
+        rospy.loginfo('Ive been toched at the front F=%s\n', rightfront_force)
         # Fill vector message
-        msg.angle = angle
-        msg.value = light_val 
-    if not back_sub.states:
+        msg.angle = (- 1023) * 180/1023
+        msg.value = -0.2 
+    if not leftfront_sub.states:
         pass
     else:
-        back_force = back_sub.states[0].total_wrench.force.x
-        rospy.loginfo('Ive been toched at the back F=%s\n', back_force) 
+        leftfront_force = leftfront_sub.states[0].total_wrench.force.x
+        rospy.loginfo('Ive been toched at the back F=%s\n', leftfront_force) 
         # Fill vector message
-        msg.angle = angle
-        msg.value = light_val 
+        msg.angle = 1023* 180/1023
+        msg.value = -0.2
 
     msg.header.stamp = rospy.Time.now()
-    pub = rospy.Publisher('force_sub', vect_msg, queue_size=10)
+    pub = rospy.Publisher('force_vect', vect_msg, queue_size=10)
     pub.publish(msg)
     rospy.loginfo('Force vector data sent')
 if  __name__ == '__main__':

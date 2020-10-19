032801526b616da6076fef2823c76760737cd967
IrisDuMutel <irisfp96@gmail.com>
2020-10-19 19:09:29 +0200

    not wroking plugin for servo

--- plugins/actuator/test.h
+++ plugins/actuator/test.h
@@ -1,9 +1,6 @@
-#ifndef GAZEBO_PLUGINS_DIFFDRIVEPLUGIN_HH_
-#define GAZEBO_PLUGINS_DIFFDRIVEPLUGIN_HH_
+// #ifndef GAZEBO_PLUGINS_MOTOR_HH_
+// #define GAZEBO_PLUGINS_MOTOR_HH_
 #include <ignition/transport/Node.hh>
-
-
-
 #include "gazebo/common/Plugin.hh"
 #include "gazebo/physics/physics.hh"
 #include "gazebo/transport/TransportTypes.hh"
@@ -14,7 +11,20 @@
 namespace gazebo
 {
 
-  class YourPlugin : public ModelPlugin
+  class YourPlugin : public ModelPlugin{
+    /// \brief Constructor
+    public: YourPlugin();
+
+    /// \brief Destructor
+    public: virtual ~YourPlugin();
+      /// Documentation inherited
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+    /// \brief Callback on world update event.
+    private: void OnUpdate();
 
 
+    private: physics::ModelPtr model;
+
+
+  };
 }

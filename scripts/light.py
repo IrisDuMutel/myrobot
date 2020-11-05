0aa903c2a3cb339cf38fa2842ac86c22fa916325
IrisDuMutel <irisfp96@gmail.com>
2020-11-05 12:45:08 +0100

    Comented loginfo

--- scripts/light.py
+++ scripts/light.py
@@ -15,15 +15,17 @@ from sensor_msgs.msg import Image, CameraInfo, Illuminance
 
 def light():
     rospy.init_node('light', anonymous=True)
-    # image_sub = message_filters.Subscriber('camera2/rgb/image_raw',Image)
-    light_sub = message_filters.Subscriber('light_sensor_plugin/lightSensor',Illuminance)
-    ts = message_filters.TimeSynchronizer([light_sub], 10)
+    light_sub = message_filters.Subscriber('/light_Sensor/light_data',Illuminance)
+    image_sub = message_filters.Subscriber('/camera2/color/image_raw',Image)
+    #image_sub is necessary for getting light values
+
+    ts = message_filters.TimeSynchronizer([light_sub,image_sub], 10)
     ts.registerCallback(callback)
     rospy.spin()
-def callback( light_sub):
+def callback( light_sub, image_sub):
     light_val = light_sub.illuminance
     # Measurement of the Photometric Illuminance in Lux.
-    rospy.loginfo('The iluminance value is %s\n', light_val)
+    # rospy.loginfo('The illuminance value is %s\n', light_val)
     msg = vect_msg()
     #TODO define here a behavior according to the lux value
     angle = 0
@@ -33,7 +35,7 @@ def callback( light_sub):
     msg.header.stamp = rospy.Time.now()
     pub = rospy.Publisher('light_vect', vect_msg, queue_size=10)
     pub.publish(msg)
-    rospy.loginfo('Light vector sent')
+    # rospy.loginfo('Light vector sent')
 if __name__ == '__main__':
     try:
         light()
